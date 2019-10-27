84885e8712ba616fd028ddaaa6584a90a33b4722
hongbo Sun <1075103615@qq.com>
2019-10-27 19:40:02 +0800

    Update python3-load.py

--- python3-load.py
+++ python3-load.py
@@ -7,6 +7,10 @@ Created on Tue Jul 30 19:35:18 2019
 
 import _pickle as cPickle
 
+list1=['hello']
+
+cPickle.dump(list1,open(".\\videowrongdict.pkl","wb"))
+
 data = cPickle.load(open(".\\videowrongdict.pkl","rb")) 
 
 print(data)
